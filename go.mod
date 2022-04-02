297a41a698ce7a54fcb6fd54533566f3373002e5
Rachel Wang <21071004+rachelwritingcode@users.noreply.github.com>
2022-04-01 21:08:18 -0400

    Add web scraper - WIP

--- go.mod
+++ go.mod
@@ -11,11 +11,23 @@ require (
 )
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/antchfx/htmlquery v1.2.4 // indirect
+	github.com/antchfx/xmlquery v1.3.10 // indirect
+	github.com/antchfx/xpath v1.2.0 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
@@ -23,4 +35,5 @@ require (
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 )
--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/buger/jsonparser"
+	"github.com/gocolly/colly"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -16,12 +18,10 @@ import (
 
 // type BookResponse struct {
 // 	Title  string `json:title`
+//	Author string `json:author`
 // 	Rating int    `json:rating`
 // 	Plot   string `json:plot`
-// }
-
-// type Book struct {
-// 	Title string `json:title`
+//	Year 	string `json:year`
 // }
 
 type MovieResponse struct {
@@ -45,7 +45,8 @@ func main() {
 
 	// Routes
 	e.GET("/getmovie", getMovie)
-	// e.GET("/getbook", getBook)
+	e.GET("/getbook", getBook)
+	// e.GET("/recommend", getRecommendation)
 
 	e.GET("/status", func(c echo.Context) error {
 		return c.HTML(
@@ -54,15 +55,9 @@ func main() {
 		)
 	})
 
-	// TODO Provide a Recommendation based on the Ratings
-	// TODO Read the book/movie
-
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
 }
-
-// func getBook(c echo.Context) error {
-// }
 
 func getMovie(c echo.Context) error {
 
@@ -123,15 +118,91 @@ func getMovie(c echo.Context) error {
 		year,
 		awards}
 
-	// TODO Retrieve Book Review Information
-
 	// Convert struct data to string
 	movieResponse, _ := json.Marshal(movieResponseData)
 	return c.String(http.StatusOK, string(movieResponse))
 
 }
 
-// func getBook(title string) string {
-// TODO Call the Book Review API
-// Pass in the title request query parameter
+func getBook(c echo.Context) error {
+	// TODO Call the Book Review API
+	// Pass in the title request query parameter
+	// Title Request Parameter
+	title := c.QueryParam("title")
+	fmt.Println("Title Query: " + title)
+
+	request_url := "http://openlibrary.org/search.json?title=" + title
+	fmt.Println("Request URL: " + request_url)
+
+	req, err := http.NewRequest("GET", request_url, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req.Header.Set("Accept", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// TODO Remove after debugging
+	open_library_id, _ := jsonparser.GetString(b, "docs", "[0]", "key")
+
+	year, _ := jsonparser.GetString(b, "docs", "[0]", "first_publish_year")
+	fmt.Println("Book Publishing Year: " + year)
+
+	author, _ := jsonparser.GetString(b, "docs", "[0]", "author_name")
+	fmt.Println("Author: " + author)
+
+	// TODO Web Scraping
+	// TODO webscrape overview information for book plot
+	// TODO webscrape the book star ratings
+
+	// Instantiate default collector
+	web_scraper := colly.NewCollector(
+		// Visit only domain
+		colly.AllowedDomains("openlibrary.org"),
+	)
+
+	// Limit the number of threads started by colly to two
+	// when visiting links which domains' matches "*httpbin.*" glob
+	web_scraper.Limit(&colly.LimitRule{
+		DomainGlob:  "*httpbin.*",
+		Parallelism: 1,
+		Delay:       20 * time.Second,
+	})
+
+	// On every a element which has href attribute call callback
+	web_scraper.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		web_scraper.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Before making a request print "Visiting ..."
+	web_scraper.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+	//TODO Set web scraping parameters!
+
+	// Start scraping the website
+	web_scraper.Visit("https://openlibrary.org/" + open_library_id)
+
+	// bookResponse, _ := json.Marshal()
+	return c.String(http.StatusOK, "Open Library URL ID: "+open_library_id)
+}
+
+// getRecommendation(c echo.Context)error{
+
 // }
