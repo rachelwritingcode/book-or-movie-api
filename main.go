3c5d877fd774ca6b4013e35677e06a72a1d52234
Rachel Wang <21071004+rachelwritingcode@users.noreply.github.com>
2022-04-10 00:12:05 -0400

    Add more validation and complete the recommendation handler function

--- main.go
+++ main.go
@@ -19,9 +19,41 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
+const openLibraryAPI = "http://openlibrary.org/search.json?title="
+const openLibraryURL = "http://openlibrary.org"
+
+// Recommendation messages
+const noBookFound string = "Watch the movie, there is no book with this title."
+const noMovieFound string = "Read the book, there is no movie with this title."
+const noBookOrMovieFound string = "There is no book or movie with this title. Try searching another title."
+const bookFirst string = "Read the book first, then the movie."
+const movieFirst string = "Watch the movie first, then the read book."
+const bothFirst string = "Watch the movie and read the book, both are evenly rated."
+
+// Error Messages
+const noBookRating string = "No rating information exists for the book."
+const noMovieRating string = "No rating information exists for the movie."
+
+// Error response
 type Error struct {
+	Status  string `json:status`
 	Code    string `json:code`
 	Message string `json:message`
+}
+
+type Recommendation struct {
+	Title          string `json:title`
+	Recommendation string `json:recommendation`
+	BookRating     string `json:bookrating`
+	MovieRating    string `json:movierating`
+	MoviePlot      string `json:movieplot`
+	BookPlot       string `json:bookplot`
+	Author         string `json: author`
+	Director       string `json:director`
+	Actors         string `json:actors`
+	BookPublished  string `json: bookpublished`
+	MovieReleased  string `json: moviereleased`
+	MovieAwards    string `json: movieawards`
 }
 
 type BookResponse struct {
@@ -51,12 +83,9 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// TODO Return the string results of /getmovie /getbook routes
-	// Return the string to the page here instead of in the handler function
-	// Use the two routes to get recommendations
 	e.GET("/getmovie", getMovie)
 	e.GET("/getbook", getBook)
-	// e.GET("/recommend", getRecommendation)
+	e.GET("/recommend", getReccomend)
 
 	e.GET("/status", func(c echo.Context) error {
 		return c.HTML(
@@ -71,9 +100,12 @@ func main() {
 
 func getMovie(c echo.Context) error {
 
-	// Title Request Parameter
 	title := c.QueryParam("title")
-	fmt.Println("Title Query: " + title)
+	movieData := getMovieData(title)
+	return c.String(http.StatusOK, movieData)
+}
+
+func getMovieData(title string) string {
 
 	// Load Environment Variables
 	err := godotenv.Load("local.env")
@@ -86,9 +118,6 @@ func getMovie(c echo.Context) error {
 	omdb_key := os.Getenv("OMDB_KEY")
 
 	request_url := omdb_api + "=" + omdb_key + "&t=" + title
-
-	fmt.Println("Request URL: " + request_url)
-
 	req, err := http.NewRequest("GET", request_url, nil)
 	if err != nil {
 		log.Fatalln(err)
@@ -108,16 +137,24 @@ func getMovie(c echo.Context) error {
 		log.Fatalln(err)
 	}
 
-	// TODO Remove after debugging
-	fmt.Println("\n OMDB API RESULTS " + string(b) + "\n")
-	// Parse out Rating
+	if strings.Contains(string(b), "Movie not found") {
+		errorResponse := &Error{
+			"Error",
+			"400",
+			"Unable to find this title",
+		}
+		errorResp, _ := json.Marshal(errorResponse)
+		errorData := string(errorResp)
+		return errorData
+	}
+
 	director, _ := jsonparser.GetString(b, "Director")
 	plot, _ := jsonparser.GetString(b, "Plot")
 	actors, _ := jsonparser.GetString(b, "Actors")
 	rating, _ := jsonparser.GetString(b, "imdbRating")
 	year, _ := jsonparser.GetString(b, "Year")
-	// TODO Escape special character & for awards field
 	awards, _ := jsonparser.GetString(b, "Awards")
+	awards = strings.ReplaceAll(awards, "\u0026", "")
 
 	movieResponseData := &MovieResponse{
 		title,
@@ -128,33 +165,13 @@ func getMovie(c echo.Context) error {
 		year,
 		awards}
 
-	// Convert struct data to string
-	movieResponse, _ := json.Marshal(movieResponseData)
-	return c.String(http.StatusOK, string(movieResponse))
-
+	movieData, _ := json.Marshal(movieResponseData)
+	return string(movieData)
 }
 
-// Add %20 to the request url
-func cleanTitleQuery(title string) string {
-	query_parameter := ""
-	title_slice := strings.Split(title, " ")
-	fmt.Println(title_slice)
+func getBookData(title string) string {
 
-	for _, s := range title_slice {
-		fmt.Println(s)
-		query_parameter += s + "%20"
-		fmt.Println("Query Parameter " + query_parameter)
-	}
-
-	return query_parameter
-}
-
-func getBook(c echo.Context) error {
-
-	originalTitle := c.QueryParam("title")
-	title := cleanTitleQuery(originalTitle)
-	request_url := "http://openlibrary.org/search.json?title=" + title
-
+	request_url := openLibraryAPI + title
 	req, err := http.NewRequest("GET", request_url, nil)
 	if err != nil {
 		log.Fatalln(err)
@@ -174,14 +191,16 @@ func getBook(c echo.Context) error {
 		log.Fatalln(err)
 	}
 
-	if strings.Contains(string(b), "400 Bad request") {
-
+	numFound, _ := jsonparser.GetInt(b, "numFound")
+	if strings.Contains(string(b), "400 Bad request") || numFound == 0 {
 		errorResponse := &Error{
+			"Error",
 			"400",
 			"Unable to find this title",
 		}
 		errorResp, _ := json.Marshal(errorResponse)
-		return c.String(http.StatusBadRequest, string(errorResp))
+		errorData := string(errorResp)
+		return errorData
 	}
 
 	open_library_id, _ := jsonparser.GetString(b, "docs", "[0]", "key")
@@ -191,7 +210,7 @@ func getBook(c echo.Context) error {
 	var plot string = ""
 	var ratings = []string{}
 	web_scraper := colly.NewCollector(
-		colly.AllowedDomains("https://openlibrary.org", "openlibrary.org"),
+		colly.AllowedDomains(openLibraryURL, "openlibrary.org"),
 	)
 
 	web_scraper.OnHTML(".readers-stats ", func(e *colly.HTMLElement) {
@@ -212,14 +231,13 @@ func getBook(c echo.Context) error {
 	})
 
 	web_scraper.OnRequest(func(request *colly.Request) {
-		fmt.Println("Visiting...https://openlibrary.org" + open_library_id)
 	})
 
-	web_scraper.Visit("https://openlibrary.org" + open_library_id)
+	web_scraper.Visit(openLibraryURL + open_library_id)
 
 	rating = strings.ReplaceAll(ratings[len(ratings)-1], "★", "")
 	rating_float, _ := strconv.ParseFloat(rating, 64)
-	rating_float = math.Ceil(rating_float*100) / 100
+	rating_float = (math.Ceil(rating_float*100) / 100) / 5 * 10
 	rating = strconv.FormatFloat(rating_float, 'E', -1, 64)
 	rating = strings.ReplaceAll(rating, "E+00", "")
 
@@ -240,8 +258,164 @@ func getBook(c echo.Context) error {
 		yearString,
 	}
 	bookResponseData, _ := json.Marshal(bookResponse)
-	return c.String(http.StatusOK, string(bookResponseData))
+	bookData := string(bookResponseData)
+	return bookData
 }
 
-// getRecommendation(c echo.Context)error{
-// }
+// Add %20 to the request url
+func cleanTitleQuery(title string) string {
+
+	query_parameter := ""
+	title_slice := strings.Split(title, " ")
+
+	for _, s := range title_slice {
+		query_parameter += s + "%20"
+	}
+
+	return query_parameter
+}
+
+func getBook(c echo.Context) error {
+
+	originalTitle := c.QueryParam("title")
+	title := cleanTitleQuery(originalTitle)
+	bookData := getBookData(title)
+	return c.String(http.StatusOK, bookData)
+}
+
+func getReccomend(c echo.Context) error {
+
+	originalTitle := c.QueryParam("title")
+	title := cleanTitleQuery(originalTitle)
+
+	bookData := getBookData(title)
+	movieData := getMovieData(title)
+
+	recommendation := compareBookAndMovieData(movieData, bookData, title)
+	status, _ := jsonparser.GetString([]byte(recommendation), "Message")
+
+	if strings.Contains(status, noBookOrMovieFound) {
+		return c.String(http.StatusNotFound, recommendation)
+	}
+	return c.String(http.StatusOK, recommendation)
+}
+
+func compareBookAndMovieData(movieData string, bookData string, title string) string {
+
+	fmt.Println("compareBookAndMovieData - start")
+	recommend := &Recommendation{}
+
+	fmt.Println("compareBookAndMovieData - book data")
+	fmt.Println(bookData + "\n")
+	fmt.Println("compareBookAndMovieData - movie data")
+	fmt.Println(movieData + "\n")
+
+	bookStatus, _ := jsonparser.GetString([]byte(bookData), "Message")
+	movieStatus, _ := jsonparser.GetString([]byte(movieData), "Message")
+
+	fmt.Println("Book Status " + bookStatus + "\n")
+	fmt.Println("Movie Status " + movieStatus + "\n")
+
+	// TODO Handle use case when ratings don't exist for book or movie
+	// Both movie and book data cannot be retrieved
+	if strings.Contains(bookStatus, "Unable to find this title") && strings.Contains(movieStatus, "Unable to find this title") {
+		errorResponse := &Error{
+			"Error",
+			"400",
+			noBookOrMovieFound,
+		}
+		errorResp, _ := json.Marshal(errorResponse)
+		errorData := string(errorResp)
+		return errorData
+
+	} else if strings.Contains(movieStatus, "Unable to find this title") { // Only book data can be retrieved
+
+		fmt.Println("Found the book but not the movie data")
+		recommend.Recommendation = noMovieFound
+		titleString, _ := jsonparser.GetString([]byte(bookData), "Title")
+		recommend.Title = titleString
+		authorString, _ := jsonparser.GetString([]byte(bookData), "Author")
+		recommend.Author = authorString
+		bookRatingString, _ := jsonparser.GetString([]byte(bookData), "Rating")
+		recommend.BookRating = bookRatingString
+		bookPlotString, _ := jsonparser.GetString([]byte(bookData), "Plot")
+		recommend.BookPlot = bookPlotString
+		publishingYear, _ := jsonparser.GetString([]byte(bookData), "Year")
+		recommend.BookPublished = publishingYear
+
+	} else if strings.Contains(bookStatus, "Unable to find this title") { // Only movie data can be retrieved
+
+		fmt.Println("Found the movie but not the book data")
+		recommend.Recommendation = noBookFound
+		titleString, _ := jsonparser.GetString([]byte(movieData), "Title")
+		recommend.Title = titleString
+		movieRatingString, _ := jsonparser.GetString([]byte(movieData), "Rating")
+		recommend.MovieRating = movieRatingString
+		actors, _ := jsonparser.GetString([]byte(movieData), "Actors")
+		recommend.Actors = actors
+		director, _ := jsonparser.GetString([]byte(movieData), "Director")
+		recommend.Director = director
+		moviePlotString, _ := jsonparser.GetString([]byte(movieData), "Plot")
+		recommend.MoviePlot = moviePlotString
+		movieYear, _ := jsonparser.GetString([]byte(movieData), "ReleaseYear")
+		recommend.MovieReleased = movieYear
+		movieAwards, _ := jsonparser.GetString([]byte(movieData), "Awards")
+		recommend.MovieAwards = movieAwards
+
+	} else { // Both book and movie data is retrieved
+
+		fmt.Println("Book and movie data is retrieved")
+		// TODO Add a success message in the response
+		// TODO Add the MovieTitle and Add the BookTitle Separately
+		// Book Data
+		titleString, _ := jsonparser.GetString([]byte(bookData), "Title")
+		recommend.Title = titleString
+		authorString, _ := jsonparser.GetString([]byte(bookData), "Author")
+		recommend.Author = authorString
+		bookRatingString, _ := jsonparser.GetString([]byte(bookData), "Rating")
+		recommend.BookRating = bookRatingString
+		bookPlotString, _ := jsonparser.GetString([]byte(bookData), "Plot")
+		recommend.BookPlot = bookPlotString
+		publishingYear, _ := jsonparser.GetString([]byte(bookData), "Year")
+		recommend.BookPublished = publishingYear
+
+		// Movie Data
+		movieRatingString, _ := jsonparser.GetString([]byte(movieData), "Rating")
+		recommend.MovieRating = movieRatingString
+		actors, _ := jsonparser.GetString([]byte(movieData), "Actors")
+		recommend.Actors = actors
+		director, _ := jsonparser.GetString([]byte(movieData), "Director")
+		recommend.Director = director
+		moviePlotString, _ := jsonparser.GetString([]byte(movieData), "Plot")
+		recommend.MoviePlot = moviePlotString
+		movieYear, _ := jsonparser.GetString([]byte(movieData), "ReleaseYear")
+		recommend.MovieReleased = movieYear
+		movieAwards, _ := jsonparser.GetString([]byte(movieData), "Awards")
+		recommend.MovieAwards = movieAwards
+
+		// Compare Movie and Book Ratings
+		bookRatingFloat, _ := strconv.ParseFloat(bookRatingString, 64)
+		movieRatingFloat, _ := strconv.ParseFloat(movieRatingString, 64)
+
+		fmt.Println("bookRatingFloat \n")
+		fmt.Println(bookRatingFloat)
+		fmt.Println("movieRatingFloat \n")
+		fmt.Println(movieRatingFloat)
+
+		if math.Max(bookRatingFloat, movieRatingFloat) == bookRatingFloat {
+			recommend.Recommendation = bookFirst
+			fmt.Println("bookRatingFloat is greater")
+		} else if math.Max(bookRatingFloat, movieRatingFloat) == movieRatingFloat {
+			recommend.Recommendation = movieFirst
+			fmt.Println("movieRatingFloat is greater")
+		} else {
+			recommend.Recommendation = bothFirst
+			fmt.Println("movies are evenly matched")
+		}
+
+	}
+
+	recommendationData, _ := json.Marshal(recommend)
+	reccommendResponse := string(recommendationData)
+	return reccommendResponse
+}
