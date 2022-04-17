e1f3389c99cf16f92b1043d64063dc1cbed7e7fe
Rachel Wang <21071004+rachelwritingcode@users.noreply.github.com>
2022-04-17 14:16:07 -0400

    Clean up messages.go

--- models/messages.go
+++ models/messages.go
@@ -1,15 +1,17 @@
 package models
 
-// Recommendation messages
-const NoBookFound string = "Watch the movie, there is no book for this title."
-const NoMovieFound string = "Read the book, there is no movie for this title."
-const NoBookOrMovieFound string = "There is no book or movie for this title. Try searching another title."
-const BookFirst string = "Read the book first, then watch the movie."
-const MovieFirst string = "Watch the movie first, then read the book."
-const BothFirst string = "Watch the movie and read the book, both are evenly rated."
+const (
+	// Recommendation messages
+	NoBookFound        string = "Watch the movie, there is no book for this title."
+	NoMovieFound       string = "Read the book, there is no movie for this title."
+	NoBookOrMovieFound string = "There is no book or movie for this title. Try searching another title."
+	BookFirst          string = "Read the book first, then watch the movie."
+	MovieFirst         string = "Watch the movie first, then read the book."
+	BothFirst          string = "Watch the movie and read the book, both are evenly rated."
 
-// Error Messages
-const NoBookRating string = "No rating information exists for the book."
-const NoMovieRating string = "No rating information exists for the movie."
-const NoBookNoMovieRating string = "No ratings exist for book or movie, watch both."
-const NoTitle string = "Unable to find this title"
+	// Error Messages
+	NoBookRating        string = "No rating information exists for the book."
+	NoMovieRating       string = "No rating information exists for the movie."
+	NoBookNoMovieRating string = "No ratings exist for book or movie, watch both."
+	NoTitle             string = "Unable to find this title"
+)
